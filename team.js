87c2d9fa921a7f68a89121596a88e0bfaacf0e6b
Jef <jefersonrevellobiz@gmail.com>
2023-06-06 16:40:17 +0200

    Create file 'team.js' containing empty array 'students'

---
+++ team.js
@@ -0,0 +1,20 @@
+// Each student must clone the repository and make their own local branch.**
+//
+//     Add an object with the following properties within the array:
+//
+//     - name
+//     - surname
+//     - age
+//     - city
+//     - hobby
+//     - favoriteFood
+//     - favoriteVideoGame
+//     - favoriteFilm
+//     - favoriteBook
+//     - petName
+//
+//     **And push the code.
+//
+//     If someone wants to leave the information empty, use the empty string: ‘’. **
+
+let students = [];
