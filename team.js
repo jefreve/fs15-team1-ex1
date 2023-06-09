87df83e8a04668aaeb5b20373de076401856fb12
Jef <jefersonrevellobiz@gmail.com>
2023-06-09 15:25:20 +0200

    Change how array of object is displayed

--- team.js
+++ team.js
@@ -1,14 +1,4 @@
 let students = [{name: "Alessandro", surname: "Ambra", age: 25, city: "Rome", hobby: "sports & coding", favoriteFood: "carbonara", favoriteVideoGame:"Final Fantasy X", favoriteFilm:"",favoriteBook:"Bersker", petName:"Guntar"}, 
-                {
-        "name" : "Jeferson", 
-        "surname" : "Revello", 
-        "age" : 32, 
-        "city" : "Andora",
-        "hobby" : "making music",
-        "favoriteFood" : "kebab",
-        "favoriteVideoGame" : "Final Fantasy X",
-        "favoriteFilm" : "The Matrix",
-        "favoriteBook" : "The 4-Hour Body by Tim Ferriss",
-        "petName" : ""
+                {name : "Jeferson", surname : "Revello", age : 32, city : "Andora", hobby : "making music", favoriteFood : "kebab", favoriteVideoGame : "Final Fantasy X", favoriteFilm : "The Matrix", favoriteBook : "The 4-Hour Body by Tim Ferriss", petName : ""
     }];
 
