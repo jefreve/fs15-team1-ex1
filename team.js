a81b2a12f3c7760f986c3d76d13d92f23c2c4633
Jef <84090230+jefreve@users.noreply.github.com>
2023-06-12 17:33:59 +0200

    Update team.js

--- team.js
+++ team.js
@@ -1,5 +1,6 @@
 let students = [{name: "Alessandro", surname: "Ambra", age: 25, city: "Rome", hobby: "sports & coding", favoriteFood: "carbonara", favoriteVideoGame:"Final Fantasy X", favoriteFilm:"",favoriteBook:"Bersker", petName:"Guntar"}, 
                 {name : "Jeferson", surname : "Revello", age : 32, city : "Andora", hobby : "making music", favoriteFood : "kebab", favoriteVideoGame : "Final Fantasy X", favoriteFilm : "The Matrix", favoriteBook : "The 4-Hour Body by Tim Ferriss", petName : ""}, 
-                {Name:"Massimo", surname:"Natella", age:30, city:"Naples", hobby:"Cinema,reading,writing", favoriteFood:"Pizza", favoriteFilm:"Once upomn time in America", favoriteBook:"Siddharta",petName:"Luna"}
-               ];
+                {Name:"Massimo", surname:"Natella", age:30, city:"Naples", hobby:"Cinema,reading,writing", favoriteFood:"Pizza", favoriteVideoGame: "GTA", favoriteFilm:"Once upomn time in America", favoriteBook:"Siddharta",petName:"Luna"}
+                {name: "Francesco", surname: "Dragonetti", age: 25, city: "Turin", hobby: "football", favoriteFood: "pizza", favoriteVideoGame: "Assassin's Creed", favoriteFilm: "Pulp Fiction", favoriteBook: "Musica per organi caldi", petName:"Nami"}; 
+];
 
